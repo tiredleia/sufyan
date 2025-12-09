7dae9564acf3778bdfaf68d2a503cb37fab4c1c5
tiredleia <razinaleia@gmail.com>
2025-12-09 14:24:30 +0800

    Add files via upload

---
+++ module2.py
@@ -0,0 +1,20 @@
+#---------------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      razin
+#
+# Created:     09/12/2025
+# Copyright:   (c) razin 2025
+# Licence:     <your licence>
+#---------------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+name = input('NAME ')
+age = input('AGE ')
+age = int(age)
+print('Hi ' + name + ', Happy ' + str(age) + 'th Birthday! I love you a lot :>')
